c8af633c46bd7663093e3c844927dd8eb9e03ec6
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 17:34:12 +0200

    Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :contacts
+    resources :sessions, only: [:create, :destroy]
   end
 end
