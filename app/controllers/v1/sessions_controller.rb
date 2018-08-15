ae164a778d4b20ab0950b44a4881e175c85c0433
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 19:04:32 +0200

    Return the authentication token

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class V1::SessionsController < ApplicationController
     user = User.where(email: params[:email]).first
 
     if user.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email]), status: :created
+      render json: user.as_json(only: [:email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end
