0ab7bc84eca4b2991b8142845db22c5019ef0fb6
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 19:03:11 +0200

    Migration to add a new authentication token field to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_150647) do
+ActiveRecord::Schema.define(version: 2018_08_15_165301) do
 
   create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_08_15_150647) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
