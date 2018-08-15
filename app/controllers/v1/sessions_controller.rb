d5c4ffb2cefca722b476ad4adf8fb33e3d90ac57
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 21:01:07 +0200

    Destroy session action

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -11,6 +11,11 @@ class V1::SessionsController < ApplicationController
   end
 
   def destroy
-    
+    current_user&.authentication_token = nil
+    if current_user&.save
+      head(:ok)
+    else
+      head(:unauthorized)
+    end
   end
 end
