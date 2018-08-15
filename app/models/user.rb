4f85684147c180667965827ea5f9df55175bd6b3
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 22:12:10 +0200

    set_jwt_token method sets the jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # random key que usaremos para pruebasen la codificación de JWT
+  # TODO hacer que sea una segura tipo Rails::Secrets.key
+  SECRET_KEY_FOR_JWT = "ahjgfjasgfjkasgfkagsfhgaskhfgashfgaksg"
+
+  def set_jwt_token
+    expiration = (Time.now + 2.weeks).to_i
+    jwt = JWT.encode(
+      { authorization_token: self.authentication_token, exp: expiration },
+      SECRET_KEY_FOR_JWT,
+      'HS256'
+    )
+
+    jwt
+  end
 end
