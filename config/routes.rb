ab8387fe551569b44b42d860179f0bd93ef2df82
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-15 19:22:07 +0200

    Json configuration for v1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :v1 do
+  namespace :v1, defaults: {format: :json} do
     resources :contacts
     resources :sessions, only: [:create, :destroy]
   end
